364bb97c74afe55669124a6738ce5d7e5343d068
Hector Aristy <rafelito266@gmail.com>
2017-02-17 21:17:42 -0400

    Testing the socket.

---
+++ src/Server/Sync_client.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Sync_client
+    {
+        static void Main(string[] args)
+        {
+            byte[] bytes = new byte[1024];
+
+            // connect to a Remote device 
+            try
+            {
+                // Establish the remote end point for the socket 
+                IPHostEntry ipHost = Dns.Resolve("45.55.77.201");
+                IPAddress ipAddr = ipHost.AddressList[0];
+                IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 1337);
+
+                Socket sender = new Socket(AddressFamily.InterNetwork,
+                                           SocketType.Stream, ProtocolType.Tcp);
+
+                // Connect the socket to the remote endpoint 
+
+                sender.Connect(ipEndPoint);
+
+                Console.WriteLine("Socket connected to {0}",
+                sender.RemoteEndPoint.ToString());
+
+                string theMessage = "This is a test";
+
+                byte[] msg = Encoding.ASCII.GetBytes(theMessage + "<theend>");
+
+                // Send the data through the socket 
+                int bytesSent = sender.Send(msg);
+
+                // Receive the response from the remote device 
+                int bytesRec = sender.Receive(bytes);
+
+                Console.WriteLine("The Server says : {0}",
+                                  Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                // Release the socket 
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+                Console.In.ReadLine();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e.ToString());
+                Console.In.ReadLine();
+            }
+        }
+    }
+}
---
+++ src/Server/Sync_server.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Sync_server
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry ipHost = Dns.Resolve("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 8080);
+
+            Socket sListener = new Socket(AddressFamily.InterNetwork,
+                                          SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                sListener.Bind(ipEndPoint);
+                sListener.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection on port {0}", ipEndPoint);
+
+                    Socket handler = sListener.Accept();
+
+                    string data = "";
+
+                    while (!data.EndsWith("\r\n\r\n"))
+                    {
+                        byte[] bytes = new byte[1024];
+
+                        int bytesRec = handler.Receive(bytes);
+
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                        if (data.IndexOf("<theend>") > -1)
+                        {
+                            break;
+                        }
+                    }
+
+                    // show the data on the console 
+                    Console.WriteLine("Text Received: {0}", data);
+
+
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                    handler.Send(msg);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.In.ReadLine();
+            }
+        }
+    }
+}
