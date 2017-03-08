39c7120b94cee362cd1c3be7bd8fc5536d54aee2
Hector Aristy <rafelito266@gmail.com>
2017-03-07 20:54:14 -0400

    Assignment 10 and 11.

--- src/PHttp/HttpServer.cs
+++ src/PHttp/HttpServer.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PHttp
@@ -10,20 +12,33 @@ namespace PHttp
     class HttpServer : IDisposable
     {
         #region properties
-        public IPEndPoint EndPoint;
+        public IPEndPoint EndPoint { get; private set; }
         public int ReadBufferSize;
         public int WriteBufferSize;
         public string ServerBanner;
         public TimeSpan ReadTimeout;
         public TimeSpan WriteTimeout;
         public TimeSpan ShutdownTimeout;
-        private HttpServerUtility ServerUtility;
-        private HttpTimeoutManager TimeoutManager;
+        public int Port { get; private set; }
+        public HttpServerState State { get { return _state; } set { if (value != _state) { _state = value; OnStateChanged(EventArgs.Empty); } } }
+
+        private TcpListener _listener;
+        internal HttpTimeoutManager TimeoutManager { get; private set; }
+        internal HttpServerUtility ServerUtility { get; private set; }
+        private AutoResetEvent _clientsChangedEvent = new AutoResetEvent(false);
+        private bool _disposed;
+        private HttpServerState _state = HttpServerState.Stopped;
         #endregion properties
 
-        public HttpServer()
+        #region Events
+        private event EventHandler StateChanged;
+        #endregion Events
+
+        public HttpServer(int port)
         {
-            EndPoint = new IPEndPoint(IPAddress.Loopback, 0);
+            Port = port;
+            State = HttpServerState.Stopped;
+            EndPoint = new IPEndPoint(IPAddress.Loopback, Port);
             ReadBufferSize = 4096;
             WriteBufferSize = 4096;
             ShutdownTimeout = TimeSpan.FromSeconds(30);
@@ -35,7 +50,28 @@ namespace PHttp
         #region methods
         public void Start()
         {
-            throw new NotImplementedException();
+            VerifyState(HttpServerState.Stopped);
+            State = HttpServerState.Starting;
+            Console.WriteLine("-- Server Starting at EndPoint {0}:{1}", EndPoint.Address, EndPoint.Port);
+            TimeoutManager = new HttpTimeoutManager(this);
+            var listener = new TcpListener(EndPoint);
+
+            try
+            {
+                listener.Start();
+                EndPoint = listener.LocalEndpoint as IPEndPoint;
+                _listener = listener;
+                ServerUtility = new HttpServerUtility();
+                Console.WriteLine("-- Server Running at EndPoint {0}:{1}", EndPoint.Address, EndPoint.Port);
+                State = HttpServerState.Started;
+                BeginAcceptTcpClient();
+            }
+            catch (Exception e)
+            {
+                State = HttpServerState.Stopped;
+                Console.WriteLine("** The Server failed to start. | Exception: " + e.Message);
+                throw new PHttpException("The Server failed to start.");
+            }
         }
         public void Stop()
         {
@@ -43,7 +79,10 @@ namespace PHttp
         }
         private void VerifyState(HttpServerState state)
         {
-            throw new NotImplementedException();
+            if (_disposed)
+                throw new ObjectDisposedException(GetType().Name);
+            if (_state != state)
+                throw new InvalidOperationException(String.Format("Expected server to be in the '{0}' state", state));
         }
         private void StopClients()
         {
@@ -51,7 +90,11 @@ namespace PHttp
         }
         private void BeginAcceptTcpClient()
         {
-            throw new NotImplementedException();
+            var listener = _listener;
+            if (listener != null)
+            {
+                listener.BeginAcceptTcpClient(AcceptTcpClientCallback, null);
+            }
         }
         private void AcceptTcpClientCallback(IAsyncResult asyncResult)
         {
@@ -67,7 +110,33 @@ namespace PHttp
         }
         public void Dispose()
         {
-            throw new NotImplementedException();
+            if(!_disposed)
+            {
+                if(_state == HttpServerState.Started)
+                {
+                    Stop();
+                }
+                if(_clientsChangedEvent != null)
+                {
+                    _clientsChangedEvent.Dispose();
+                    _clientsChangedEvent = null;
+                }
+                if(TimeoutManager != null)
+                {
+                    TimeoutManager.Dispose();
+                    TimeoutManager = null;
+                }
+                _disposed = true;
+            }
+            //throw new NotImplementedException();
+        }
+
+        protected virtual void OnStateChanged(EventArgs e)
+        {
+            if(StateChanged != null)
+            {
+                StateChanged(this, e);
+            }
         }
         #endregion methods
     }
